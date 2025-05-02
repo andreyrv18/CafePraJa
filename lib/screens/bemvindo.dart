9658061d139275f0e27a1f2e928e9687459d9ad7
Andreyrvs <andreyrvdev@gmail.com>
2025-05-02 09:49:31 -0300

    refactor: altera a cor de fundo da tela de boas-vindas para usar o tema do aplicativo

--- lib/screens/bemvindo.dart
+++ lib/screens/bemvindo.dart
@@ -6,7 +6,7 @@ class Bemvindo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff38241D),
+      backgroundColor: Theme.of(context).colorScheme.primary,
       body: Column(
         spacing: 16,
         children: [
