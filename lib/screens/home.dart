ce877dd96863c5d2c9c7578669391372763e8514
Andreyrvs <andreyrvdev@gmail.com>
2025-05-02 14:07:06 -0300

    refactor: remove widgets CafeListView, AppBar e CafeBottomNavigationBar do arquivo home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,69 +1,8 @@
-import 'package:cafe_pra_ja/theme.dart';
+import 'package:cafe_pra_ja/widgets/app_bar_widget.dart';
 import 'package:flutter/material.dart';
-
-class CafeListView extends StatefulWidget {
-  const CafeListView({super.key});
-
-  @override
-  State<CafeListView> createState() => _CafeListViewState();
-}
-
-class _CafeListViewState extends State<CafeListView> {
-  final List<String> items = [
-    "cafe",
-    "capuchinno",
-    "cha",
-    "mocha",
-    "cafe Frio",
-    "gelado",
-    "cha amargo",
-    "mochazão",
-  ];
-
-  int selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.only(left: 8),
-      itemCount: items.length,
-      scrollDirection: Axis.horizontal,
-      itemBuilder: (context, index) {
-        final isSelected = index == selectedIndex;
-        final item = items[index];
-
-        return GestureDetector(
-          onTap: () {
-            setState(() {
-              selectedIndex = index;
-            });
-          },
-
-          child: Container(
-            width: 120,
-            decoration: BoxDecoration(
-              color: isSelected ? Color(0xffC67C4E) : Colors.transparent,
-              border: Border.all(color: Colors.black12),
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-            ),
-            margin: EdgeInsets.symmetric(horizontal: 8),
-            child: Center(
-              child: Text(
-                item,
-                style:
-                    isSelected
-                        ? cafePraJaTheme.textTheme.titleMedium?.copyWith(
-                          color: Colors.white,
-                        )
-                        : cafePraJaTheme.textTheme.titleMedium,
-              ),
-            ),
-          ),
-        );
-      },
-    );
-  }
-}
+import 'package:cafe_pra_ja/theme.dart';
+import 'package:cafe_pra_ja/widgets/cafe_bottom_navigation_bar.dart';
+import 'package:cafe_pra_ja/widgets/cafe_list_view.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -87,72 +26,9 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: cafePraJaTheme.bottomNavigationBarTheme.backgroundColor,
-      appBar: appBar(),
+      appBar: AppBarWidget(),
       body: corpo(),
       bottomNavigationBar: CafeBottomNavigationBar(),
-    );
-  }
-
-  AppBar appBar() {
-    return AppBar(
-      flexibleSpace: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color(0xFF313131), Color(0xFF131313)],
-            begin: Alignment.bottomLeft,
-            end: Alignment.topRight,
-          ),
-        ),
-      ),
-      bottom: PreferredSize(
-        preferredSize: const Size.fromHeight(20.0),
-        child: Transform.translate(
-          offset: const Offset(0, -10), // Move o conteúdo 25 pixels para cima
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "localização",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontFamily: "Sora",
-                      ),
-                    ),
-                    Text(
-                      "Pinhais, Paraná",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontFamily: "Sora",
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 44,
-                  width: 44,
-
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Icon(
-                      Icons.account_circle,
-                      color: Colors.white,
-                      size: 44,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
     );
   }
 
@@ -175,42 +51,6 @@ class MyHomePage extends StatelessWidget {
         SizedBox(height: 25, child: CafeListView()),
         Expanded(child: SizedBox(child: Text("Fim"))),
       ],
-    );
-  }
-}
-
-class CafeBottomNavigationBar extends StatefulWidget {
-  const CafeBottomNavigationBar({super.key});
-  @override
-  State<CafeBottomNavigationBar> createState() =>
-      _CafeBottomNavigationBarState();
-}
-
-class _CafeBottomNavigationBarState extends State<CafeBottomNavigationBar> {
-  int _selectedIndex = 0;
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.shopping_cart_checkout),
-          label: "checkout",
-        ),
-        BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Favorito"),
-      ],
-      currentIndex: _selectedIndex,
-      selectedItemColor:
-          cafePraJaTheme.bottomNavigationBarTheme.selectedItemColor,
-      unselectedItemColor:
-          cafePraJaTheme.bottomNavigationBarTheme.unselectedItemColor,
-      onTap: _onItemTapped,
     );
   }
 }
