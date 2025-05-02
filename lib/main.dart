c30abd835f33d351555430f257c8bfe2a5c9ef8c
Andreyrvs <andreyrvdev@gmail.com>
2025-05-02 09:49:20 -0300

    refactor: adiciona tema ao aplicativo na configuração do MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:cafe_pra_ja/screens/bemvindo.dart';
 import 'package:cafe_pra_ja/screens/home.dart';
+import 'package:cafe_pra_ja/theme.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      theme: cafePraJaTheme,
       home: const Bemvindo(title: "Bem Vindo"),
       routes: <String, WidgetBuilder>{
         'home': (BuildContext context) => const MyApp(),
