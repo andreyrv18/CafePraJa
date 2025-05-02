b70c9a3c125683de8799396356eeb7c780c1cce1
Andreyrvs <andreyrvdev@gmail.com>
2025-05-02 13:57:15 -0300

    refactor: adiciona barra de navegação inferior com opções de navegação

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -89,6 +89,7 @@ class MyHomePage extends StatelessWidget {
       backgroundColor: cafePraJaTheme.bottomNavigationBarTheme.backgroundColor,
       appBar: appBar(),
       body: corpo(),
+      bottomNavigationBar: CafeBottomNavigationBar(),
     );
   }
 
@@ -174,6 +175,42 @@ class MyHomePage extends StatelessWidget {
         SizedBox(height: 25, child: CafeListView()),
         Expanded(child: SizedBox(child: Text("Fim"))),
       ],
+    );
+  }
+}
+
+class CafeBottomNavigationBar extends StatefulWidget {
+  const CafeBottomNavigationBar({super.key});
+  @override
+  State<CafeBottomNavigationBar> createState() =>
+      _CafeBottomNavigationBarState();
+}
+
+class _CafeBottomNavigationBarState extends State<CafeBottomNavigationBar> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.shopping_cart_checkout),
+          label: "checkout",
+        ),
+        BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Favorito"),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor:
+          cafePraJaTheme.bottomNavigationBarTheme.selectedItemColor,
+      unselectedItemColor:
+          cafePraJaTheme.bottomNavigationBarTheme.unselectedItemColor,
+      onTap: _onItemTapped,
     );
   }
 }
