785d65efbc6ec77b4e81db2020e2cb9f16d46f44
Andreyrvs <andreyrvdev@gmail.com>
2025-05-02 14:06:11 -0300

    refactor: adiciona widget AppBar personalizado com gradiente e informações de localização

---
+++ lib/widgets/app_bar_widget.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
+  const AppBarWidget({super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(80.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      flexibleSpace: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF313131), Color(0xFF131313)],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+          ),
+        ),
+      ),
+      bottom: PreferredSize(
+        preferredSize: const Size.fromHeight(20.0),
+        child: Transform.translate(
+          offset: const Offset(0, -10), // Move o conteúdo 25 pixels para cima
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "localização",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontFamily: "Sora",
+                      ),
+                    ),
+                    Text(
+                      "Pinhais, Paraná",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontFamily: "Sora",
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 44,
+                  width: 44,
+
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Icon(
+                      Icons.account_circle,
+                      color: Colors.white,
+                      size: 44,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
