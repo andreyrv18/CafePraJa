7de6accd6eedb107fc35c3d7058fe145b6268079
Andreyrvs <andreyrvdev@gmail.com>
2025-05-02 19:05:44 -0300

    refactor: remove linhas em branco e ajusta a formatação no tema

--- lib/theme.dart
+++ lib/theme.dart
@@ -279,7 +279,6 @@ class MaterialTheme {
   ThemeData darkMediumContrast() {
     return theme(darkMediumContrastScheme());
   }
-  
 
   static ColorScheme darkHighContrastScheme() {
     return const ColorScheme(
@@ -336,22 +335,19 @@ class MaterialTheme {
     return theme(darkHighContrastScheme());
   }
 
-
   ThemeData theme(ColorScheme colorScheme) => ThemeData(
-     useMaterial3: true,
-     brightness: colorScheme.brightness,
-     colorScheme: colorScheme,
-     textTheme: textTheme.apply(
-       bodyColor: colorScheme.onSurface,
-       displayColor: colorScheme.onSurface,
-     ),
-     scaffoldBackgroundColor: colorScheme.background,
-     canvasColor: colorScheme.surface,
+    useMaterial3: true,
+    brightness: colorScheme.brightness,
+    colorScheme: colorScheme,
+    textTheme: textTheme.apply(
+      bodyColor: colorScheme.onSurface,
+      displayColor: colorScheme.onSurface,
+    ),
+    scaffoldBackgroundColor: colorScheme.background,
+    canvasColor: colorScheme.surface,
   );
 
-
-  List<ExtendedColor> get extendedColors => [
-  ];
+  List<ExtendedColor> get extendedColors => [];
 }
 
 class ExtendedColor {
