77bedf1fb08a0d1145929d0d7cae5b22a7ea2175
Andreyrvs <andreyrvdev@gmail.com>
2025-05-02 16:47:39 -0300

    refactor: substitui tema personalizado por tema padrão do Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:cafe_pra_ja/screens/bemvindo.dart';
 import 'package:cafe_pra_ja/screens/home.dart';
-import 'package:cafe_pra_ja/theme.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      theme: cafePraJaTheme,
+      theme: ThemeData.light(useMaterial3: true),
       home: const Bemvindo(title: "Bem Vindo"),
       routes: <String, WidgetBuilder>{
         'home': (BuildContext context) => const MyApp(),
