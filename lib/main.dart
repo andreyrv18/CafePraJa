b5359c110ee70c249c26ea31cd8970bba932100a
Andrey Visniewski <andreyrv18@gmail.com>
2025-05-02 01:06:24 -0300

    refactor: muda a pagina inicial para pasta de screens deixando o main focado no roteamento

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:ultra_coffee/screens/bemvindo.dart';
+import 'package:ultra_coffee/screens/home.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: const Bemvindo(),
+      home: const Bemvindo(title: "Bem Vindo"),
       routes: <String, WidgetBuilder>{
         'home': (BuildContext context) => const MyApp(),
       },
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Text("main"));
-  }
 }
