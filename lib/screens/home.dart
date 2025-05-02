fca4144da6ebec8990cf2018c05ac057fd94b420
Andrey Visniewski <andreyrv18@gmail.com>
2025-05-02 01:05:27 -0300

    feature: Adiciona a classe MyApp e MyHomePage com estrutura básica

---
+++ lib/screens/home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Coffee Pinhais',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+      ),
+      home: const MyHomePage(title: 'Home'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text("main"));
+  }
+}
