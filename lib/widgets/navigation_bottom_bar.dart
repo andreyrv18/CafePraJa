32ec5f1e55e90db0c72f6c92669a47d0f4b9f58f
Andreyrvs <andreyrvdev@gmail.com>
2025-05-03 01:21:45 -0300

    refactor: atualiza a barra de navegação inferior para usar NavigationBar e simplifica a estrutura das páginas

--- lib/widgets/navigation_bottom_bar.dart
+++ lib/widgets/navigation_bottom_bar.dart
@@ -1,3 +1,4 @@
+import 'package:cafe_pra_ja/screens/home.dart';
 import 'package:flutter/material.dart';
 
 class NavigationBottomBar extends StatefulWidget {
@@ -10,66 +11,66 @@ class NavigationBottomBar extends StatefulWidget {
 class _NavigationBottomBarState extends State<NavigationBottomBar> {
   int currentPageIndex = 0;
 
+  final List<Widget> pages = [
+    MyHomePage(title: "Home"),
+    Center(child: Text("Notifications Page", style: TextStyle(fontSize: 24))),
+    Center(child: Text("Messages Page", style: TextStyle(fontSize: 24))),
+  ];
+
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        backgroundColor: theme.colorScheme.surface,
-        onDestinationSelected:
-            (int index) => {
-              setState(() {
-                currentPageIndex = index;
-              }),
-            },
-        indicatorColor: theme.colorScheme.secondaryContainer,
-        selectedIndex: currentPageIndex,
-        destinations: <Widget>[
-          NavigationDestination(
-            selectedIcon: Icon(
-              Icons.home_sharp,
-              color: theme.colorScheme.onSecondaryContainer,
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Expanded(
+          child: pages[currentPageIndex], // Exibe a página correspondente
+        ),
+        NavigationBar(
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index; // Atualiza o índice da página
+            });
+          },
+          indicatorColor: theme.colorScheme.secondaryContainer,
+          selectedIndex: currentPageIndex,
+          destinations: <Widget>[
+            NavigationDestination(
+              selectedIcon: Icon(
+                Icons.home_sharp,
+                color: theme.colorScheme.onSecondaryContainer,
+              ),
+              icon: Icon(
+                Icons.home_outlined,
+                color: theme.colorScheme.onSurfaceVariant,
+              ),
+              label: "Home",
             ),
-            icon: Icon(
-              Icons.home_outlined,
-              color: theme.colorScheme.onSurfaceVariant,
-            ),
-            label: "Home",
-          ),
-          NavigationDestination(
-            selectedIcon: Badge(
-              child: Icon(
+            NavigationDestination(
+              selectedIcon: Icon(
                 Icons.notifications_sharp,
                 color: theme.colorScheme.onSecondaryContainer,
               ),
-            ),
-            icon: Badge(
-              child: Icon(
+              icon: Icon(
                 Icons.notifications_outlined,
                 color: theme.colorScheme.onSurfaceVariant,
               ),
+              label: "Notifications",
             ),
-            label: "Notifications",
-          ),
-          NavigationDestination(
-            selectedIcon: Badge(
-              label: Text('2'),
-              child: Icon(
+            NavigationDestination(
+              selectedIcon: Icon(
                 Icons.messenger_sharp,
                 color: theme.colorScheme.onSecondaryContainer,
               ),
-            ),
-            icon: Badge(
-              label: Text('2'),
-              child: Icon(
+              icon: Icon(
                 Icons.messenger_outline,
                 color: theme.colorScheme.onSurfaceVariant,
               ),
+              label: "Messages",
             ),
-            label: "messages",
-          ),
-        ],
-      ),
+          ],
+        ),
+      ],
     );
   }
 }
