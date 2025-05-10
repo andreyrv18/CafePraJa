76e3aacaf0cee7d31d24c44055e341a7573e9b8c
Andreyrvs <andreyrvdev@gmail.com>
2025-05-09 23:59:52 -0300

    refactor: simplifica o widget AppBarWidget e melhora a legibilidade do código

--- lib/widgets/app_bar_widget.dart
+++ lib/widgets/app_bar_widget.dart
@@ -8,64 +8,49 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
     return AppBar(
       flexibleSpace: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [
-              Theme.of(context).colorScheme.secondary,
-              Theme.of(context).colorScheme.onSurface,
-            ],
+            colors: [theme.colorScheme.secondary, theme.colorScheme.onSurface],
             begin: Alignment.bottomLeft,
             end: Alignment.topRight,
           ),
         ),
       ),
       actions: [
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: SizedBox(
-            height: 44,
-            width: 44,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Icon(
-                Icons.account_circle,
-                color: Theme.of(context).colorScheme.primary,
-                size: 44,
-              ),
-            ),
-          ),
+        IconButton(
+          icon: Icon(Icons.account_circle),
+          onPressed: () {},
+          color: theme.colorScheme.onSecondary,
+          iconSize: 44,
+          alignment: Alignment.center,
         ),
       ],
-      title: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "localização",
-                  style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                    color: Theme.of(context).colorScheme.onPrimary,
-                  ),
+      title: Row(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "localização",
+                style: theme.textTheme.titleSmall?.copyWith(
+                  color: theme.colorScheme.onPrimary,
                 ),
-                Text(
-                  "Pinhais, Paraná",
-                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                    color: Theme.of(context).colorScheme.onPrimary,
-                  ),
+              ),
+              Text(
+                "Pinhais, Paraná",
+                style: theme.textTheme.titleMedium?.copyWith(
+                  color: theme.colorScheme.onPrimary,
                 ),
-              ],
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
