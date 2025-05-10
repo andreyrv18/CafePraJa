71cc83b437f8e23c02874976ae29ab6c5ee93966
Alan <alanjeferson53@gmail.com>
2025-05-10 15:37:28 -0300

    tela de login no perfil

--- lib/screens/perfil.dart
+++ lib/screens/perfil.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Perfil extends StatefulWidget {
@@ -10,6 +11,100 @@ class Perfil extends StatefulWidget {
 class _PerfilState extends State<Perfil> {
   @override
   Widget build(BuildContext context) {
-    return Column(children: [Text("Bem vindo"), Text("Deu certo")]);
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(elevation: 0, backgroundColor: Colors.transparent),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(27),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF4B2E1D), // marrom escuro
+              Color(0xFFBFA082), // bege quente
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 30),
+            const Text(
+              "Digite os dados de acesso nos campos abaixo.",
+              style: TextStyle(color: Color(0xFFF5E1C2)), // creme
+            ),
+            const SizedBox(height: 30),
+            const CupertinoTextField(
+              cursorColor: Color(0xFF7B4F2F), // marrom médio
+              padding: EdgeInsets.all(15),
+              placeholder: "Digite o seu e-mail",
+              placeholderStyle: TextStyle(
+                color: Color(0xFFD2BBA0),
+                fontSize: 14,
+              ), // bege claro
+              style: TextStyle(color: Colors.white, fontSize: 14),
+              decoration: BoxDecoration(
+                color: Color(0x332D1B0B), // marrom com transparência
+                borderRadius: BorderRadius.all(Radius.circular(7)),
+              ),
+            ),
+            const SizedBox(height: 5),
+            const CupertinoTextField(
+              padding: EdgeInsets.all(15),
+              cursorColor: Color(0xFF8B5E3C),
+              placeholder: "Digite sua senha",
+              obscureText: true,
+              placeholderStyle: TextStyle(
+                color: Color(0xFFD2BBA0),
+                fontSize: 14,
+              ),
+              style: TextStyle(color: Colors.white, fontSize: 14),
+              decoration: BoxDecoration(
+                color: Color(0x332D1B0B),
+                borderRadius: BorderRadius.all(Radius.circular(7)),
+              ),
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+              width: double.infinity,
+              child: CupertinoButton(
+                padding: const EdgeInsets.all(17),
+                color: Color(0xFF6E3B1E), // marrom café com leite
+                child: const Text(
+                  "Acessar",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ),
+            const SizedBox(height: 7),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                border: Border.all(color: Color(0xFFF5E1C2), width: 0.8),
+                borderRadius: BorderRadius.circular(7),
+              ),
+              child: CupertinoButton(
+                child: const Text(
+                  "Crie sua conta",
+                  style: TextStyle(
+                    color: Color(0xFFF5E1C2),
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
