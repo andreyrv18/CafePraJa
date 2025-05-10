cb794e94d1890259202cd759435fb91455176cd3
Andreyrvs <andreyrvdev@gmail.com>
2025-05-10 00:00:02 -0300

    refactor: reorganiza as páginas no NavigationBottomBar e melhora a legibilidade do código

--- lib/widgets/navigation_bottom_bar.dart
+++ lib/widgets/navigation_bottom_bar.dart
@@ -16,51 +16,55 @@ class _NavigationBottomBarState extends State<NavigationBottomBar> {
   final List<Widget> pages = [
     Scaffold(appBar: AppBarWidget(), body: MyHomePage(title: "Home")),
     Scaffold(
+      appBar: AppBar(title: const Text("Checkout")),
+      body: const Center(child: Text("Página de Checkout")),
+    ),
+    Scaffold(
       appBar: AppBar(title: const Text("Notificações")),
       body: const Center(child: Text("Página de Notificações")),
     ),
-    Scaffold(
-      appBar: AppBar(title: const Text("Mensagens")),
-      body: const Center(child: Text("Página de Mensagens")),
-    ),
     Scaffold(appBar: AppBar(title: const Text("Perfil")), body: Perfil()),
   ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      currentPageIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     return Scaffold(
       body: pages[currentPageIndex],
+
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentPageIndex,
-        onTap: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
+        onTap: _onItemTapped,
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
-            activeIcon: Icon(Icons.home_sharp),
+            activeIcon: Icon(Icons.home_filled),
             label: "Home",
             backgroundColor: theme.colorScheme.primary,
           ),
           BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            activeIcon: Icon(Icons.shopping_cart_checkout_outlined),
+            label: "Checkout",
+          ),
+          BottomNavigationBarItem(
             icon: Icon(Icons.notifications_outlined),
-            activeIcon: Icon(Icons.notifications_sharp),
+            activeIcon: Icon(Icons.notifications_active_rounded),
             label: "Notificações",
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.messenger_outline),
-            activeIcon: Icon(Icons.messenger_sharp),
-            label: "Mensagens",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            activeIcon: Icon(Icons.person_sharp),
+            icon: Icon(Icons.person_outline),
+            activeIcon: Icon(Icons.person_4_sharp),
             label: "Perfil",
           ),
         ],
+        selectedIconTheme: IconThemeData(color: theme.colorScheme.onPrimary),
       ),
     );
   }
