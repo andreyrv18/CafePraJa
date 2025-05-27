f273e6dddca2294b88b5daeba2d63d9ac3a0443d
Andreyrvs <andreyrvdev@gmail.com>
2025-05-27 18:00:39 -0300

    Refatoração da página Home
    A página `Home` foi refatorada para utilizar o widget `Cardapio` em vez de `CafeGridView`. A estrutura da página foi simplificada, removendo o método `corpo` e integrando sua lógica diretamente no método `build`.

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,4 @@
-import 'package:cafe_pra_ja/screens/home/widgets/cafe_grid_view.dart';
+import 'package:cafe_pra_ja/firestore/cardapio.dart';
 import 'package:cafe_pra_ja/screens/home/widgets/list_view_cafe.dart';
 import 'package:flutter/material.dart';
 
@@ -9,34 +9,30 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      body: corpo(context),
-    );
-  }
-
-  Column corpo(BuildContext context) {
     final ColorScheme theme = Theme.of(context).colorScheme;
     final TextTheme textTheme = Theme.of(context).textTheme;
-    return Column(
-      children: [
-        Center(
-          child: SizedBox(
-            width: 315,
-            child: TextField(
-              decoration: InputDecoration(
-                hintStyle: textTheme.labelMedium,
-                prefixIcon: Icon(Icons.search, color: theme.onSurface),
-                hintText: "Encontre seu café favorito!",
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Column(
+        children: [
+          Center(
+            child: SizedBox(
+              width: 315,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintStyle: textTheme.labelMedium,
+                  prefixIcon: Icon(Icons.search, color: theme.onSurface),
+                  hintText: "Encontre seu café favorito!",
+                ),
               ),
             ),
           ),
-        ),
-        SizedBox(height: 12),
-        SizedBox(height: 24, child: CafeListView()),
-        SizedBox(height: 12),
-        Expanded(child: CafeGridView()),
-      ],
+          SizedBox(height: 12),
+          SizedBox(height: 24, child: CafeListView()),
+          SizedBox(height: 12),
+          Expanded(child: Cardapio()),
+        ],
+      ),
     );
   }
 }
