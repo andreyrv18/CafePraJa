5a6347c7ad734dd50e77b59b4164c54ad2862152
Andreyrvs <andreyrvdev@gmail.com>
2025-05-26 23:12:35 -0300

    Refactor: Atualiza importação da tela de checkout
    A importação da tela de checkout foi atualizada para refletir a nova estrutura de diretórios.

--- lib/widgets/navigation_bottom_bar.dart
+++ lib/widgets/navigation_bottom_bar.dart
@@ -1,5 +1,5 @@
 import 'package:cafe_pra_ja/screens/cupons.dart';
-import 'package:cafe_pra_ja/screens/checkout.dart';
+import 'package:cafe_pra_ja/screens/checkout/checkout.dart';
 import 'package:cafe_pra_ja/screens/home/home.dart';
 import 'package:cafe_pra_ja/screens/perfil.dart';
 import 'package:cafe_pra_ja/widgets/app_bar_widget.dart';
