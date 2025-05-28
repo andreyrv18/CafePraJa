ebe232b3efb57a3ee8e71677f15de3f5319e2ddd
Andreyrvs <andreyrvdev@gmail.com>
2025-05-28 00:33:13 -0300

    Refactor: Remove Firestore dependency from app state
    The `DatabaseService` dependency and its initialization have been removed from `ApplicationState`.

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,13 +1,9 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
-import 'firestore/database_service.dart';
-
 class ApplicationState extends ChangeNotifier {
   ApplicationState() {
     init();
   }
-  Future<void> init() async {
-    var _reference = await DatabaseService();
-  }
+  Future<void> init() async {}
 }
