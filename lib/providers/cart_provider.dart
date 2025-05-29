dcfae0c242d989b044075479b861330afb975b36
Andreyrvs <andreyrvdev@gmail.com>
2025-05-29 02:42:14 -0300

    Refatora `CartProvider` para usar `Stream` na recuperação de itens do carrinho
    A classe `CartProvider` foi atualizada para utilizar um `Stream` para buscar os itens do carrinho de compras.
    
    - O construtor `CartProvider()` agora inicializa o `_dbService` sem chamar `recuperarItensCarrinho()` imediatamente.
    - O método `getItensDoCarrinhoStream()` foi adicionado para retornar um `Stream<List<CartItemModel>>`. Este método utiliza `_dbService.getCartItemsStream()` e mapeia o `QuerySnapshot` para uma lista de `CartItemModel`.
    - Uma verificação foi adicionada em `getItensDoCarrinhoStream()` para retornar um `Stream.empty()` caso `_dbService` seja nulo, prevenindo potenciais erros.
    - O método `recuperarItensCarrinho()` foi removido.
    - O método `adicionarItem()` agora recebe os parâmetros individualmente (`idProduto`, `nomeProduto`, `precoUnitario`, `imagemUrl`) em vez de um objeto `produto`.

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,25 +1,45 @@
+import 'package:cafe_pra_ja/models/cart_item_model.dart';
+import 'package:cafe_pra_ja/models/menu_item_model.dart';
 import 'package:cafe_pra_ja/services/database_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 
 class CartProvider with ChangeNotifier {
   late DatabaseService _dbService;
   static const String _testeUidFixo = "dqoFBe79WG5ZUv1qC6i3";
+  final DatabaseService _databaseService = DatabaseService();
 
-  CartProvier() {
+  CartProvider() {
     _dbService = DatabaseService(uid: _testeUidFixo);
-    recuperarItensCarrinho();
   }
 
-  Future<void> recuperarItensCarrinho() async {
-    await _dbService.getCartItemsStream();
+  Stream<List<CartItemModel>> getItensDoCarrinhoStream() {
+    if (_dbService == null) {
+      print(
+        "ALERTA: _dbService é null em getItensDoCarrinhoStream. Isso não deveria acontecer.",
+      );
+      return Stream.empty(); // Retorna um stream vazio para evitar crash
+    }
+    return _dbService.getCartItemsStream().map((querySnapshot) {
+      return querySnapshot.docs.map((doc) {
+        return CartItemModel.fromFirestore(
+          doc as DocumentSnapshot<Map<String, dynamic>>,
+        );
+      }).toList();
+    });
   }
 
-  Future<void> adicionarItem(produto) async {
+  Future<void> adicionarItem(
+    idProduto,
+    nomeProduto,
+    precoUnitario,
+    imagemUrl,
+  ) async {
     await _dbService.addOrUpdateItemNoCarrinho(
-      produto.idProduto,
-      produto.nomeProduto,
-      produto.precoUnitario,
-      produto.imagemUrl,
+      idProduto,
+      nomeProduto,
+      precoUnitario,
+      imagemUrl,
     );
   }
 
