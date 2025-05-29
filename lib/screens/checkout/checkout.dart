789e4cd8819c55d858db42f0d0f217a3fc917951
Andreyrvs <andreyrvdev@gmail.com>
2025-05-29 01:03:58 -0300

    Refatora tela de checkout para usar StreamProvider
    A tela de checkout foi refatorada para utilizar um `StreamProvider` para gerenciar a lista de itens do carrinho. Anteriormente, a tela utilizava um `StatefulWidget` e `context.watch<ApplicationState>()` para acessar os dados do carrinho.
    
    Essa mudança simplifica o gerenciamento de estado e torna o código mais reativo às atualizações do carrinho.
    
    Além disso, a exibição dos itens do carrinho foi aprimorada para mostrar o nome do produto e o preço unitário em um `ListTile`. A mensagem de "Carrinho de compras vazio" foi removida, pois o `StreamProvider` cuidará de exibir a tela vazia quando não houver itens.

--- lib/screens/checkout/checkout.dart
+++ lib/screens/checkout/checkout.dart
@@ -1,43 +1,29 @@
-import 'package:cafe_pra_ja/app_state.dart';
+import 'package:cafe_pra_ja/models/cart_item_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class Checkout extends StatefulWidget {
+class Checkout extends StatelessWidget {
   const Checkout({super.key});
 
-  @override
-  State<Checkout> createState() => _CheckoutState();
-}
-
-class _CheckoutState extends State<Checkout> {
   @override
   Widget build(BuildContext context) {
     final ColorScheme theme = Theme.of(context).colorScheme;
     final TextTheme textTheme = Theme.of(context).textTheme;
-    var appState = context.watch<ApplicationState>();
-
-    if (appState.cartList.isEmpty) {
-      return Center(child: Text("Carrinho de compras vazio"));
-    }
+    final List<CartItemModel> cartItems = context.watch<List<CartItemModel>>();
     return Column(
       children: [
-        Expanded(
+        Card(
+          color: theme.tertiary,
           child: ListView.builder(
-            itemCount: appState.cartList.length,
-            itemBuilder: (BuildContext context, int index) {
-              var cartItem = appState.cartList[index];
-
-              return Column(
-                children: [
-                  Text("nome "),
-                  IconButton(
-                    onPressed: () async {
-                      // await DatabaseService().getCardapioCompleto();
-                      appState.shoppingCart(cartItem);
-                    },
-                    icon: Icon(Icons.remove_shopping_cart),
-                  ),
-                ],
+            itemBuilder: (context, item) {
+              final nomeProduto = cartItems[item].nomeProduto;
+              final precoUnitario = cartItems[item].precoUnitario;
+              return ListTile(
+                title: Text(
+                  nomeProduto,
+                  style: TextStyle(fontSize: textTheme.titleMedium?.fontSize),
+                ),
+                subtitle: Text(precoUnitario.toStringAsFixed(2)),
               );
             },
           ),
