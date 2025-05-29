0b543db570deaf78529d5ff4f683068ceaa26d15
Andreyrvs <andreyrvdev@gmail.com>
2025-05-29 02:42:22 -0300

    Exibe itens do carrinho e adiciona funcionalidade para aumentar e diminuir quantidade
    A tela de checkout agora exibe os itens presentes no carrinho utilizando um `ListView.builder`. Para cada item, são mostrados o nome, preço unitário, quantidade e o subtotal.
    
    Além disso, foram adicionados botões de "+" e "-" que permitem ao usuário aumentar ou diminuir a quantidade de cada item no carrinho, utilizando o `CartProvider` para atualizar o estado.
    
    Caso o carrinho esteja vazio, uma mensagem "Seu carrinho está vazio." é exibida.

--- lib/screens/checkout/checkout.dart
+++ lib/screens/checkout/checkout.dart
@@ -1,4 +1,5 @@
 import 'package:cafe_pra_ja/models/cart_item_model.dart';
+import 'package:cafe_pra_ja/providers/cart_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -10,20 +11,63 @@ class Checkout extends StatelessWidget {
     final ColorScheme theme = Theme.of(context).colorScheme;
     final TextTheme textTheme = Theme.of(context).textTheme;
     final List<CartItemModel> cartItems = context.watch<List<CartItemModel>>();
+    final maisItem = context.watch<CartProvider>();
+    if (cartItems.isEmpty) {
+      return Center(
+        child: Column(children: [const Text("Seu carrinho está vazio.")]),
+      );
+    }
     return Column(
       children: [
-        Card(
-          color: theme.tertiary,
+        Expanded(
           child: ListView.builder(
-            itemBuilder: (context, item) {
-              final nomeProduto = cartItems[item].nomeProduto;
-              final precoUnitario = cartItems[item].precoUnitario;
-              return ListTile(
-                title: Text(
-                  nomeProduto,
-                  style: TextStyle(fontSize: textTheme.titleMedium?.fontSize),
-                ),
-                subtitle: Text(precoUnitario.toStringAsFixed(2)),
+            itemCount: cartItems.length,
+            itemBuilder: (context, index) {
+              final item = cartItems[index]; // Pega o item atual
+              final nomeProduto = item.nomeProduto;
+              final precoUnitario = item.precoUnitario;
+              final quantidade = item.quantidade;
+              return Column(
+                children: [
+                  ListTile(
+                    title: Text(
+                      nomeProduto,
+                      style: textTheme.titleMedium?.copyWith(
+                        color: theme.onSurface,
+                      ),
+                    ),
+                    subtitle: Text('Qtd: $quantidade x R\$ $precoUnitario'),
+                    trailing: Text(
+                      'R\$ ${(precoUnitario * quantidade).toStringAsFixed(2)}',
+                      style: textTheme.titleSmall?.copyWith(
+                        color: theme.primary,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          maisItem.atualizarQuantidade(
+                            item.id,
+                            item.quantidade + 1,
+                          );
+                        },
+                        icon: Icon(Icons.add),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          maisItem.atualizarQuantidade(
+                            item.id,
+                            item.quantidade - 1,
+                          );
+                        },
+                        icon: Icon(Icons.remove),
+                      ),
+                    ],
+                  ),
+                ],
               );
             },
           ),
