00f52f2968df3e3e4d85d59a03899a132eeac9a9
Andreyrvs <andreyrvdev@gmail.com>
2025-05-30 00:24:42 -0300

    feat: cria provider para cupons
    O novo provider `CuponsProvider` é responsável por gerenciar a lógica de negócios relacionada aos cupons. Ele utiliza o `DatabaseService` para buscar um stream de todos os cupons do Firestore e mapeá-los para objetos `CuponsItemModel`.
    
    A inclusão de uma mensagem de log (`print`) ajuda a verificar a quantidade de cupons recebidos e mapeados.

---
+++ lib/providers/cupons_provider.dart
@@ -0,0 +1,21 @@
+import 'package:cafe_pra_ja/models/cupons_item_model.dart';
+import 'package:cafe_pra_ja/services/database_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+
+class CuponsProvider with ChangeNotifier {
+  late DatabaseService _dbService;
+
+  CuponsProvider() : _dbService = DatabaseService();
+
+  Stream<List<CuponsItemModel>> get cuponsStream {
+    return _dbService.getTodosOsCuponsStream().map((querySnapshot) {
+      final List<CuponsItemModel> listaDeCupons =
+          querySnapshot.docs.map((doc) {
+            return CuponsItemModel.fromFirestore(doc);
+          }).toList();
+      print("💰 Cupons recebidos e mapeados: ${listaDeCupons.length} itens");
+      return listaDeCupons;
+    });
+  }
+}
