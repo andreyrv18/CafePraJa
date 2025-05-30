9769a7f32456433bbc66cd5b1041a785a550c8ff
Andreyrvs <andreyrvdev@gmail.com>
2025-05-30 00:24:24 -0300

    Implementa provider de cupons
    O `CuponsProvider` foi adicionado para gerenciar os cupons disponíveis no aplicativo, utilizando `StreamProvider` para fornecer um fluxo de dados de cupons.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:cafe_pra_ja/models/cart_item_model.dart';
 import 'package:cafe_pra_ja/providers/cart_provider.dart';
+import 'package:cafe_pra_ja/providers/cupons_provider.dart';
 import 'package:cafe_pra_ja/providers/menu_provider.dart';
 import 'package:cafe_pra_ja/widgets/navigation_bottom_bar.dart';
-import 'package:flutter/material.dart';
+import 'package:cafe_pra_ja/models/cupons_item_model.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'theme.dart';
 import 'util.dart';
@@ -22,10 +24,11 @@ void main() async {
                   context.read<CartProvider>().getItensDoCarrinhoStream(),
           initialData: const [],
         ),
-        ChangeNotifierProvider(
-          create: (_) {
-            return MenuProvider();
-          },
+        ChangeNotifierProvider(create: (_) => MenuProvider()),
+        ChangeNotifierProvider(create: (_) => CuponsProvider()),
+        StreamProvider<List<CuponsItemModel>>(
+          create: (context) => context.read<CuponsProvider>().cuponsStream,
+          initialData: const [],
         ),
       ],
       child: MyApp(),
