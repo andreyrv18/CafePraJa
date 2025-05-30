d2633504c8e082038257b12befe08ab1a38ebaab
Andreyrvs <andreyrvdev@gmail.com>
2025-05-30 00:24:53 -0300

    feat: Cria model `CuponsItemModel`
    Cria o model `CuponsItemModel` para representar os cupons.
    
    Este model inclui os seguintes campos:
    - `id`: Identificador único do cupom.
    - `codigo`: Código do cupom.
    - `porcentagem`: Porcentagem de desconto do cupom.
    
    Adiciona o método `fromFirestore` para converter um `DocumentSnapshot` do Firestore em uma instância de `CuponsItemModel`.

---
+++ lib/models/cupons_item_model.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class CuponsItemModel {
+  final String id;
+  final String codigo;
+  final int porcentagem;
+
+  CuponsItemModel({
+    required this.id,
+    required this.codigo,
+    required this.porcentagem,
+  });
+
+  factory CuponsItemModel.fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> doc,
+  ) {
+    Map<String, dynamic> data = doc.data()!;
+    return CuponsItemModel(
+      id: doc.id,
+      codigo: data['codigo'] as String? ?? 'CÓDIGO INVÁLIDO',
+      porcentagem: (data['porcentagem'] as num? ?? 0).toInt(),
+    );
+  }
+}
