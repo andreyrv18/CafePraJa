9ae3de3996d96e3003857dc1145df12680f478dc
Andreyrvs <andreyrvdev@gmail.com>
2025-06-07 01:56:08 -0300

    Cria classe de autenticação
    A classe `Autenticacao` foi criada como um `StatelessWidget` para lidar com a autenticação de usuários.

--- lib/autentication.dart
+++ lib/autentication.dart
@@ -1,12 +1,11 @@
-
-
 import 'package:flutter/cupertino.dart';
 
 class Autenticacao extends StatelessWidget {
+  const Autenticacao({super.key});
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     throw UnimplementedError();
   }
-  
 }
