61e99594d56da32cd783953458d36a8904d0004c
Andreyrvs <andreyrvdev@gmail.com>
2025-07-03 23:49:45 -0300

    FIX - Ajusta imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
-import 'package:flutter/material.dart';
-
+import 'package:cafe_pra_ja/app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
 import 'firebase_options.dart';
 
-import 'package:cafe_pra_ja/app.dart';
-
 void main() async {
-  // Bloc.observer = const AppBlocObserver();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
-  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
