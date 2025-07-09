632d6ae2ef37591c9858d980c24d9e178a9564bb
Andreyrvdev <andreyrv18@gmail.com>
2025-07-08 22:35:33 -0300

    FIX - Adiciona botão para voltar a pagina inicial

--- lib/ui/error/error_screeen.dart
+++ lib/ui/error/error_screeen.dart
@@ -1,12 +1,25 @@
+import 'package:cafe_pra_ja/routing/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class ErrorScreen extends StatelessWidget {
-  const ErrorScreen({super.key});
+class NotFoundScreen extends StatelessWidget {
+  const NotFoundScreen({super.key, required this.uri});
+
+  final String uri;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Erro: Algo de errado aconteceu!!")),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page Not Found')),
+      body: Column(
+        children: [
+          Center(child: Text("Can't find a page for: $uri")),
+          ElevatedButton(
+            onPressed: () => context.go(Routes.initial),
+            child: const Text('Go Home'),
+          ),
+        ],
+      ),
     );
   }
 }
