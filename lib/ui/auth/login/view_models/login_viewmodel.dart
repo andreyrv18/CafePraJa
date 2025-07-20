3153fc7e620afb3cf0f2ff13def9e392fc7ee75a
Andreyrvdev <andreyrv18@gmail.com>
2025-07-20 16:31:06 -0300

    HOTFIX - Ajusta assets

--- lib/ui/auth/login/view_models/login_viewmodel.dart
+++ lib/ui/auth/login/view_models/login_viewmodel.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart'
     hide EmailAuthProvider, PhoneAuthProvider;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
-import 'package:flutter/materialart';
+import 'package:flutter/material.dart';
 
 class LoginViewModel extends ChangeNotifier {
   LoginViewModel() {
